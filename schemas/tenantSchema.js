0014488bc272e2e332d10d449b2b63645bbae73a
ImaadM097 <99783026+ImaadM097@users.noreply.github.com>
2023-07-13 13:25:49 +0530

    Minor changes /tenantSchema.js

--- schemas/tenantSchema.js
+++ schemas/tenantSchema.js
@@ -3,7 +3,11 @@ require('dotenv').config();
 const uri = process.env.MONGODB_URL;
 
 async function connect() {
-    mongoose.connect(uri);
+    mongoose.connect(uri, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        dbName: 'SuperAdminPortal',
+      });
 }
 connect().then(()=>console.log('connected')).catch((e)=>console.log(e));
 const tenantSchema = new mongoose.Schema({
@@ -18,3 +22,14 @@ const tenantSchema = new mongoose.Schema({
     }
 });
 
+// const Tenant = mongoose.model('Tenant', tenantSchema);
+// const myntra = new Tenant({name: 'Myntra',
+//     domain: 'myntra.com',
+//     active: true,
+//     features: {
+//         volumeControlEnabled: true,
+//         productDrawerEnabled: true,
+//         reportEnabled: true,
+//         likeEnabled: true
+// }})
+// myntra.save();
