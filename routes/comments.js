239ad3fa03c83e1efedf32d4d5aa69572c418bfb
ImaadM097 <99783026+ImaadM097@users.noreply.github.com>
2023-07-16 13:11:28 +0530

    Update comments.js
    Added search functionality

--- routes/comments.js
+++ routes/comments.js
@@ -3,10 +3,30 @@ const router = express.Router();
 const comment = require('../schemas/commentSchema')
 const authenticateToken = require('../auth/authenticateToken')   //to be implemented
 router.use(authenticateToken)
+const searchFields = ['channel_name', 'title'];
+//'comment_count','inappropriate_comment_count'
 
 router.get('/list', authenticateToken, async(req,res)=>{
-    const allComments = await comment.find({});
-    res.json(allComments);
+    const query = req.query;
+    
+    if(Object.keys(query).length === 0) {
+        const allComments = await comment.find({});
+        res.json(allComments);
+    }
+    else {
+        
+        const searchTerm = query['search'];
+        if(typeof searchTerm !== undefined) {
+            const filter = []
+            for(let i=0; i<searchFields.length; i++) {
+                filter.push({[searchFields[i]]  : searchTerm});
+            }
+            console.log(filter)
+            const result = await comment.find({$or: filter});
+            res.json(result);
+        }
+        else res.json({});
+    }
 })
 
 router.post('/create', authenticateToken, async(req,res)=>{
